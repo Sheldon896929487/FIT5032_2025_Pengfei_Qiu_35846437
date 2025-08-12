f5e6268d1478a8c2a70b0eaf80de2a896f61cc23
Sheldon89692947 <pqiu0002@student.monash.edu>
2025-08-12 22:20:25 +0800

    Update GitHub Actions to deploy from week-4-end branch

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/FIT5032_2025_Pengfei_Qiu_35846437/'
-      : '/'
-  }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/FIT5032_2025_Pengfei_Qiu_35846437/'
+    : '/'
+}
